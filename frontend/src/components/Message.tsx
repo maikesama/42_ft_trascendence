ee1b79538423a642ec1d9e840d74d7c59f21039e
Lorenzo Iafigliola <liafigli@e4r7p3.42roma.it>
2022-09-11 12:26:08 +0200

    Chat+ MessageSent/Received(component)

---
+++ frontend/src/components/Message.tsx
@@ -0,0 +1,35 @@
+import Nav from 'react-bootstrap/Nav';
+import Text from 'react';
+import './css/Navbar.css';
+//import icon from 'bootstrap-icons';
+import axios from 'axios'
+import "./css/Message.css"
+
+export function MessageSent(props:any) {
+
+
+    return (
+    <>
+    <div className="container-message">
+    <img src="https://www.w3schools.com/w3images/avatar_g2.jpg" alt="Avatar" />
+        
+    <p>{props.message}</p>
+    <span className="time-right">{props.time}</span>
+    </div>
+    </>
+	);
+}
+
+export function MessageReceived(props:any) {
+
+
+    return (
+    <>
+    <div className="container-message darker">
+    <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar" className="right" />
+    <p>{props.message}</p>
+    <span className="time-left">{props.time}</span>
+    </div>
+    </>
+	);
+}
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,9 +1,23 @@
 import {Header} from '../components/Header'
-
+import {MessageSent, MessageReceived} from '../components/Message'
+import '../components/css/Chat.css'
 export const Chat = () => {
+
+    const title = {
+        marginTop: '2%',
+        marginBottom: '1%',
+        color : 'white',
+        fontFamily: 'MyWebFont'
+    }
+
     return (
         <>
         <Header/>
+        <h3 style={title}>Chat</h3>
+        <div className="chat-box">
+        <MessageSent message="Hey come va?" time="11.00"/>
+        <MessageReceived message="Ma tutt appost e tu?" time="11.02" />
+        </div>
         </>
     );
 }
