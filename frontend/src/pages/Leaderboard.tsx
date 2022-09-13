a00607a4e2a715a6447476b9c6e2e9c6c9c6d703
Lorenzo Iafigliola <liafigli@e4r14p2.42roma.it>
2022-09-13 16:34:40 +0200

    .

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import {Navbar} from '../components/Navbar';
 import {Header} from '../components/Header';
+import { LeaderboardHeader } from "../components/LeaderboardHeader";
+import { LeaderboardItem } from "../components/LeaderboardItem";
+
 
 import '../font/font.css';
 
@@ -23,7 +26,11 @@ export const Leaderboard = () => {
 
 	return (
         <>
-        <Header />
+        <Header/>
+        <LeaderboardHeader id="Profile" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="Nickname" guilds="Guilds" score="Score" status="Status"/>
+        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="liafigli" guilds="42Dragons" score="1123" status="online"/>
+        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="pippo" guilds="42Dragons" score="345" status="offline"/>
+        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="hdede" guilds="42Dragons" score="3475" status="error"/>
         </>
 	);
 }
--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -1,16 +1,16 @@
 import {LeaderboardItem} from '../components/LeaderboardItem'
 import {LeaderboardHeader} from '../components/LeaderboardHeader'
 import {Header} from '../components/Header'
+import background from '../images/bg_room.jpg'
 
 
 export const Test = () => {
 	return (
         <>
-        <Header/>
-        <LeaderboardHeader id="Profile" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="Nickname" guilds="Guilds" score="Score" status="Status"/>
-        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="liafigli" guilds="42Dragons" score="1123" status="online"/>
-        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="pippo" guilds="42Dragons" score="345" status="offline"/>
-        <LeaderboardItem id="1" image="https://www.w3schools.com/w3images/avatar_g2.jpg" nickname="hdede" guilds="42Dragons" score="3475" status="error"/>
+        <Header />
+        <div style={{height:"100vh", backgroundImage: 'url(' + background + ')' ,backgroundSize: 'cover', imageResolution: '100%', position: 'relative'}}>
+.sdfsdfdfsd
+        </div>
         </>
 	);
 }
