fd1c36b84cc9f8364f51ad66cbffd9ffd5f45a60
iafi01 <lorenzoiafigliola@gmail.com>
2022-09-16 16:13:08 +0200

    homepage update

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import { Form } from "react-bootstrap";
 import {Navbar} from '../components/Navbar';
 import {Header} from '../components/Header';
 import {Modality} from '../components/Modality';
-
+import headerImage from '../images/2.jpg';
 import '../font/font.css';
 import { url } from "inspector";
 import background from '../images/bg_room.jpg'
@@ -49,7 +49,29 @@ export const Homepage = () => {
     const pongMod = "https://i.gifer.com/QgxJ.gif";
 
     const modalityBox = {
-         height: '100%', border: '3px solid black', opacity: 0.8
+        backgroundImage: `url(https://i.pinimg.com/originals/32/48/ce/3248cee8c0f7cb165130337f560e9043.gif)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: '100%',
+        height: '100%',
+        border: '3px solid black',
+        opacity: 0.8
+    }
+    const modalityBox2 = {
+        backgroundImage: `url(https://i.pinimg.com/originals/32/48/ce/3248cee8c0f7cb165130337f560e9043.gif)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: '100%',
+        rotate: '180deg',
+         height: '100%',
+         border: '3px solid black',
+         opacity: 0.8
+    }
+
+    const home = {
+        backgroundImage: `url(${headerImage})`,
+        height: '100%',
+        fontFamily: 'MyWebFont',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: '100% 100%',
     }
     //d-flex justify-content-evenly align-items-center mw-100
     //w-50 h-50 d-inline-block
@@ -59,19 +81,18 @@ export const Homepage = () => {
         <>
         <Header/>
         <div className="container-fluid " style={{height: `calc(100vh - ${headerHeight})`, backgroundColor: 'grey', backgroundSize: '100%', }}>
-            <div className="row " style={{height: '20vh'}}>
-                <div className="col-md-12 d-flex justify-content-center align-items-center" style={{backgroundColor: 'lightgrey',height: '100%', fontFamily: 'MyWebFont'}}>
-                    <h1>TRANSCENDENCE PONG GAME</h1>
+            <div className="row " style={{height: '100%'}}>
+                <div className="col-md-12 d-flex justify-content-center align-items-center" style={home}>
+                    <div>
+                        <h1 style={{background: 'white' , borderRadius: 35, padding: 15, opacity: 0.85}}>TRANSCENDENCE</h1><br /><br />
+                        <div style={{fontFamily: 'Arial'}} className="d-flex flex-row justify-content-center align-items-center">
+                            <Modality name="Classico" page="classico"/>
+                            <Modality name="Invertito" page="invertito"/>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="row" style={{height: `calc(80vh - ${headerHeight}) `}}>
-                <div className="col-md-6" style={modalityBox}>
-                    FT_TRANSCENDENCE PONG GAME
-                </div>
-                <div className="col-md-6" style={modalityBox}>
-                    FT_TRANSCENDENCE PONG GAME
-                </div>
-            </div>
+            
         </div>
         </>
 	);
