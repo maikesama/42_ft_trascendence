88df94ee65a29dc674f50b4f9efca8b4ee3b6277
iafi01 <lorenzoiafigliola@gmail.com>
2022-09-20 11:01:34 +0200

    added css Homepage was const{style}

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -16,71 +16,7 @@ let logged = false;
 
 export const Homepage = () => {
 
-    const headerHeight = '150px';
-
-    const modality = {
-        color : 'white',
-        fontFamily: 'MyWebFont'
-    }
-    const linkModality = {
-        transform: 'none',
-        color : 'trasparent',
-        textDecoration: 'none'
-    }
-    const menu = {
-        backgroundColor: 'white',
-        marginLeft:15,
-        marginRight:5,
-        width:'15%',
-        height: '10%',
-        display: 'none'
-    }
-    const lightText = {
-        color: 'white',
-        backgroundColor : '#510ac2',
-        borderRadius: 4,
-        padding: 15,
-        marginTop: '2%'
-    }
-
-    const submitButton = {
-        width: '25%',
-        marginTop: 15
-    }
-
-    const pongMod = "https://i.gifer.com/QgxJ.gif";
-
-    const modalityBox = {
-        backgroundImage: `url(https://i.pinimg.com/originals/32/48/ce/3248cee8c0f7cb165130337f560e9043.gif)`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: '100%',
-        height: '100%',
-        border: '3px solid black',
-        opacity: 0.8
-    }
-    const modalityBox2 = {
-        backgroundImage: `url(https://i.pinimg.com/originals/32/48/ce/3248cee8c0f7cb165130337f560e9043.gif)`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: '100%',
-        rotate: '180deg',
-         height: '100%',
-         border: '3px solid black',
-         opacity: 0.8
-    }
-
-    const thebody = {
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-    }
-
-    const home = {
-        backgroundImage: `url(${headerImage})`,
-        height: '100%',
-        fontFamily: 'MyWebFont',
-        backgroundRepeat: "no-repeat",
-        backgroundSize: '100% 100%',
-    }
+    
     //https://img.freepik.com/free-vector/vector-cartoon-background-quest-room-with-closed-doors_33099-1202.jpg
     //d-flex justify-content-evenly align-items-center mw-100
     //w-50 h-50 d-inline-block
@@ -90,7 +26,7 @@ export const Homepage = () => {
         <>
         <Header/>
         <div className="container-fluid " id="bodybox">
-                <div className="row" style={thebody}>
+                <div className="row" id="row">
                     <div className="col-md-6 d-flex justify-content-center align-items-center" id="classic">
                         <h1 className="modalityName">Classico</h1> 
                     </div>
