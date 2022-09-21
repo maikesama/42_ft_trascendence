271f8636043e18994f75231099f76f5714cce5ee
iafi01 <lorenzoiafigliola@gmail.com>
2022-09-21 15:33:33 +0200

    Admin TextBox Grid init

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -4,6 +4,22 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import {Navbar} from '../components/Navbar';
 import {Header} from '../components/Header';
+import { Grid } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import '../font/font.css';
 
@@ -20,10 +36,27 @@ export const Admin = () => {
         textDecoration: 'none'
     }
     
+    const darkTheme = createTheme({
+        palette: {
+          mode: 'dark',
+          primary: {
+            main: '#1976d2',
+          },
+        },
+      });
 
 	return (
         <>
         <Header />
-        </>
+        <AppBar position="static" style={{backgroundColor: 'transparent', border: 0, /*boxShadow: 'none',*/ height: 180, justifyContent: 'center', alignItems: 'left'}}>
+        <Container maxWidth="xl">
+        <Grid container spacing={2}>
+            <Grid item md={12}>
+                <TextField sx={{backgroundColor: 'white', borderRadius: 15, width: '20%'}} id="filled-helperText" label="Search..." type="search" variant="filled" />
+            </Grid>
+        </Grid>
+      </Container>
+    </AppBar>
+    </>
 	);
 }
