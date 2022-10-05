09b1012aad730774cb7304647db4e779145b1d09
tizianoaureli <taureli2000@gmail.com>
2022-10-05 19:50:13 +0200

    Modified profile page

--- frontend/src/components/ProfileContain.tsx
+++ frontend/src/components/ProfileContain.tsx
@@ -11,52 +11,56 @@ import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TextField from '@mui/material/TextField';
-import { ProfileEdit } from "./ProfileEdit";
+import Divider from '@mui/material/Divider';
+import { MatchEdit, ProfileEdit, SocialEdit } from "./ProfileEdit";
 
 
 import "./css/ProfileEdit.css"
 
 export const ProfileContain = (props: any) => {
-  
-    const colors = {
-        color: 'black', backgroundColor: 'white'
-    }
-    
-    const MatchesFriends = {
-        color: 'black', backgroundColor: 'white',
-        display: 'flex', alignItems: 'center', justifyContents: 'center'
-    }
+
+  const colors = {
+    color: 'black', backgroundColor: 'white'
+  }
+
+  const MatchesFriends = {
+    color: 'black', backgroundColor: 'white',
+    display: 'flex', alignItems: 'center', justifyContents: 'center'
+  }
 
   return (
-    <Grid container spacing={2} style={{marginTop: 50}}>
-    <Grid item xs={4} style={colors}>
-    General Rank
-    </Grid>
-    <Grid item xs={4} style={colors}>
-    Victories
-    </Grid>
-    <Grid item xs={4} style={colors}>
-    Ladder victories
-    </Grid>
-    {/* Punti row */}
-    <Grid item xs={4} style={colors}>
-    12
-    </Grid>
-    <Grid item xs={4} style={colors}>
-    34
-    </Grid>
-    <Grid item xs={4} style={colors}>
-    18
-    </Grid>
-    {/* Side edit profile */}
-    <Grid item xs={4} style={colors}>
-    <ProfileEdit/>
-    </Grid>
-    {/* Matches and Friends */}
-    <Grid item xs={8} style={MatchesFriends}>
-    <ProfileEdit/>
-    <ProfileEdit/>
-    </Grid>
+    <Grid container spacing={4} style={{marginTop: 25 }}>
+      <Grid item xs={4} style={colors}>
+        General Rank
+      </Grid>
+      <Grid item xs={4} style={colors}>
+        Victories
+      </Grid>
+      <Grid item xs={4} style={colors}>
+        Ladder victories
+      </Grid>
+      {/* Punti row */}
+      <Grid item xs={4} style={colors}>
+        12
+        <Divider />
+      </Grid>
+      <Grid item xs={4} style={colors}>
+        34
+        <Divider />
+      </Grid>
+      <Grid item xs={4} style={colors}>
+        18
+        <Divider />
+      </Grid>
+      {/* Side edit profile */}
+      <Grid item xs={4} style={colors} >
+        <ProfileEdit />
+      </Grid>
+      {/* Matches and Friends */}
+      <Grid item xs={8} style={MatchesFriends} justifyContent='space-around'>
+        <SocialEdit />
+        <MatchEdit />
+      </Grid>
     </Grid>
   );
 }
--- frontend/src/components/ProfileEdit.tsx
+++ frontend/src/components/ProfileEdit.tsx
@@ -11,13 +11,77 @@ import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 
 
 import "./css/ProfileEdit.css"
+import { Input } from '@mui/material';
+
+export const MatchEdit = (props: any) => {
+  return (
+
+    <Card sx={{ minWidth: 400, height: 600 }}>
+      <CardContent>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Nickname:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Username:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+          <Button sx={{ color: 'black', visibility: 'hidden' }} >
+            <EditIcon />
+          </Button>
+        </div>
+
+      </CardContent>
+      <CardActions sx={{ justifyContent: 'center' }}>
+        <Button>Save</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export const SocialEdit = (props: any) => {
+  return (
+
+    <Card sx={{ minWidth: 400, height: 600 }}>
+      <CardContent>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Nickname:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Username:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+        </div>
+
+      </CardContent>
+      <CardActions sx={{ justifyContent: 'center' }}>
+        <Button>Save</Button>
+      </CardActions>
+    </Card>
+    
+  );
+}
 
 export const ProfileEdit = (props: any) => {
-  
-  function handleNick(){
+
+  function handleNick() {
     const inputbox = document.getElementById('txtNick');
     inputbox?.removeAttribute('disabled')
     inputbox?.setAttribute('placeholder', 'Inserisci Nickname');
@@ -25,55 +89,53 @@ export const ProfileEdit = (props: any) => {
 
   return (
     <Grid
-  container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  style={{ marginTop:50 }}
- >
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      style={{ marginTop: 50 }}
+    >
 
-  <Grid item xs={3}>
-        <Card sx={{ minWidth: 400, height: 600 }}>
-        <Button className="UploadImageBtn" component="label" sx={{width: '100%', height: '380px', padding: 0}}>
-        <CardMedia
-            component="img"
-            height="380"
-            image="https://cdn.intra.42.fr/users/liafigli.jpeg"
-            alt=""
-        />
-        <Typography className="UploadImageTxt">Upload Image</Typography>
-        <input type="file" hidden/>
+      <Grid item xs={3} style={{maxWidth: '300px'}}>
+        <Button className="UploadImageBtn" component="label" sx={{ width: '100%', height: '180px', padding: 0, alignItems: 'flex-start' }} >
+          <Avatar alt="Remy Sharp" src="https://cdn.intra.42.fr/users/liafigli.jpeg" sx={{ width: 175, height: 175 }}/>
+          <Typography className="UploadImageTxt">Upload Image</Typography>
+          <input type="file" hidden />
         </Button>
-        
-        <CardContent>
-          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            
-            <Typography variant="h5" component="div" sx={{marginTop: 2, marginRight: 2}}>
-            Nickname: 
-            </Typography>
-            <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
-            <Button sx={{color: 'black'}} onClick={handleNick}>
-            <EditIcon/>
-            </Button>
-          </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
-          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            
-            <Typography variant="h5" component="div" sx={{marginTop: 2, marginRight: 2}}>
-            Username: 
-            </Typography>
-            <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
-            <Button sx={{color: 'black', visibility: 'hidden'}} >
-            <EditIcon/>
-            </Button>
-          </div>
-            
-        </CardContent>
-        <CardActions sx={{justifyContent: 'center'}}>
-            <Button>Save</Button>
-        </CardActions>
-        </Card>
-        </Grid>
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Nickname:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+          <Button sx={{ color: 'black' }} onClick={handleNick}>
+            <EditIcon />
+          </Button>
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Username:
+          </Typography>
+          <TextField id="txtNick" placeholder="liafigli" variant="standard" disabled />
+          <Button sx={{ color: 'black', visibility: 'hidden' }} >
+            <EditIcon />
+          </Button>
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+            Score:
+          </Typography>
+          <TextField id="txtNick" placeholder="224" variant="standard" disabled />
+          <Button sx={{ color: 'black', visibility: 'hidden' }} >
+            <EditIcon />
+          </Button>
+        </div>
+        <Button variant="contained" >Save</Button>
+      </Grid>
     </Grid>
   );
 }
