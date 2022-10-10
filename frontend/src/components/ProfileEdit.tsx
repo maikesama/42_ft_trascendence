d2c5d6c049c5847663238d169210d35b5b1d8056
Tiziano Aureli <tizianoaureli@Tizianos-MacBook-Air.local>
2022-10-10 13:55:04 +0200

    Added visualization of match result

--- frontend/src/components/ProfileEdit.tsx
+++ frontend/src/components/ProfileEdit.tsx
@@ -60,11 +60,11 @@ export const SocialEdit = (props: any) => {
 
     return (
       <ListItem style={style} key={index} >
-        <Avatar />
-        <ListItemText primary={`Item ${index + 1}`}/>
+        <Avatar style={{border: "2px solid green"}}/>
+        <ListItemText primary={`You`} />
         <ListItemText primary={`VS`} />
         <ListItemText primary={`Item ${index + 1}`} />
-        <Avatar />
+        <Avatar style={{border: "2px solid red"}}/>
       </ListItem>
     );
   }
@@ -74,7 +74,7 @@ export const SocialEdit = (props: any) => {
 
     return (
       <ListItem style={style} key={index} >
-        <Avatar/>
+        <Avatar />
         <ListItemText primary={`Item ${index + 1}`} />
         <i style={{ fontSize: 8, color: 'green' }} className="bi bi-circle-fill"></i>
       </ListItem>
@@ -86,11 +86,13 @@ export const SocialEdit = (props: any) => {
 
     return (
       <ListItem style={style} key={index} >
-        <Avatar />
-        <ListItemText primary={`Item ${index + 1}`}/>
+        <Avatar style={{border: "2px solid green"}}/>
+        <ListItemText primary={`You`} />
         <ListItemText primary={`VS`} />
         <ListItemText primary={`Item ${index + 1}`} />
-        <Avatar />
+        <Avatar style={{border: "2px solid red"}}/>
+        <Divider variant="middle"/>
+        <Button variant="outlined" size='small'>Match Info</Button>
       </ListItem>
     );
   }
@@ -100,89 +102,90 @@ export const SocialEdit = (props: any) => {
 
     return (
       <ListItem style={style} key={index} >
-        <Avatar/>
-        <ListItemText primary={`Item ${index + 1}`} />
-        <i style={{ fontSize: 8, color: 'green' }} className="bi bi-circle-fill"></i>
-        <Button variant="outlined">Visit</Button>
-        <Button variant="outlined">Unfriend</Button>
-        <Button variant="contained">Block</Button>
+        <Avatar />
+        <ListItemText primary={`Item`} />
+        <i style={{ fontSize: 8, color: 'green' }} className="bi bi-circle-fill"/>
+        <Divider variant="middle"/>
+        <Button variant="outlined" size='small'>Visit</Button>
+        <Button variant="outlined" size='small' style={{color: '#fc0000', borderColor: '#fc0000'}}>Unfriend</Button>
+        <Button variant="contained" size='small' style={{backgroundColor: '#fc0000', borderColor: '#fc0000'}}>Block</Button>
       </ListItem>
     );
   }
 
   return (
     <div>
-    <Card sx={{ minWidth: 400, height: 600, borderRadius: 10, boxShadow: '0px 0px 0px 1px #D0D0D0' }}>
-      <CardContent>
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Card sx={{ minWidth: 400, height: 600, borderRadius: 10, boxShadow: '0px 0px 0px 1px #D0D0D0' }}>
+        <CardContent>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
-          <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
-            {props.title}
-          </Typography>
-          <label style={{ color: "#fff", backgroundColor: 'black', borderRadius: '35px 35px', padding: 6 }}>{props.tot}</label>
-        </div>
+            <Typography variant="h5" component="div" sx={{ marginTop: 2, marginRight: 2 }}>
+              {props.title}
+            </Typography>
+            <label style={{ color: "#fff", backgroundColor: 'black', borderRadius: '35px 35px', padding: 6 }}>{props.tot}</label>
+          </div>
 
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', textAlignLast: 'center' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', textAlignLast: 'center' }}>
 
-          <FixedSizeList
+            <FixedSizeList
 
-            height={400}
-            width={300}
-            itemSize={80}
-            itemCount={5}
-            overscanCount={5}
-          >
-            {props.matches ? renderSocialRowPreview : renderMatchesRowPreview}
-          </FixedSizeList>
-        </div>
+              height={400}
+              width={300}
+              itemSize={80}
+              itemCount={5}
+              overscanCount={5}
+            >
+              {props.matches ? renderSocialRowPreview : renderMatchesRowPreview}
+            </FixedSizeList>
+          </div>
 
-      </CardContent>
-      <CardActions sx={{ justifyContent: 'center' }}>
-        <Button onClick={props.matches ? handleClickOpenFriendsList : handleClickOpenMatchesList}>Explore</Button>
-      </CardActions>
-    </Card>
-    {/*Friends List Modal*/}
-    <Dialog open={openFriendsList} onClose={handleCloseFriendsList}>
-      <DialogTitle textAlign="center">Friends List</DialogTitle>
-      <DialogContent>
-        <div style={{textAlignLast:'center'}}>
-        <FixedSizeList
-          height={400}
-          width={600}
-          itemSize={80}
-          itemCount={5} /*Qui deve essere restituito il numero di amici nella lista*/
-          overscanCount={5}
-          >
-          {renderSocialRow}
-        </FixedSizeList>
-        </div>
-          <DialogActions>
-          <Button variant="outlined">Refresh</Button>
-          <Button variant="contained" onClick={handleCloseFriendsList}>Close</Button>
-      </DialogActions>
-      </DialogContent>
-    </Dialog>
-    {/*matches List Modal*/}
-    <Dialog open={openMatchesList} onClose={handleCloseMatchesList}>
-      <DialogTitle textAlign="center">Matches List</DialogTitle>
-      <DialogContent>
-      <div style={{textAlignLast:'center'}}>
-        <FixedSizeList
-          height={400}
-          width={300}
-          itemSize={80}
-          itemCount={5} /*Qui deve essere restituito il numero di match completati*/
-          overscanCount={5}
-          >
-          {renderMatchesRow}
-        </FixedSizeList>
-        </div>
-        <DialogActions>
-          <Button variant="outlined">Refresh</Button>
-          <Button variant="contained" onClick={handleCloseMatchesList}>Close</Button>
-        </DialogActions>
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'center' }}>
+          <Button onClick={props.matches ? handleClickOpenFriendsList : handleClickOpenMatchesList}>Explore</Button>
+        </CardActions>
+      </Card>
+      {/*Friends List Modal*/}
+      <Dialog open={openFriendsList} onClose={handleCloseFriendsList}>
+        <DialogTitle textAlign="center">Friends List</DialogTitle>
+        <DialogContent>
+          <div style={{ textAlignLast: 'center' }}>
+            <FixedSizeList
+              height={400}
+              width={400}
+              itemSize={80}
+              itemCount={5} /*Qui deve essere restituito il numero di amici nella lista*/
+              overscanCount={5}
+            >
+              {renderSocialRow}
+            </FixedSizeList>
+          </div>
+          <DialogActions style={{ justifyContent: 'center' }}>
+            <Button variant="outlined">Refresh</Button>
+            <Button variant="contained" onClick={handleCloseFriendsList}>Close</Button>
+          </DialogActions>
         </DialogContent>
-    </Dialog>
+      </Dialog>
+      {/*matches List Modal*/}
+      <Dialog open={openMatchesList} onClose={handleCloseMatchesList}>
+        <DialogTitle textAlign="center">Matches List</DialogTitle>
+        <DialogContent>
+          <div style={{ textAlignLast: 'center' }}>
+            <FixedSizeList
+              height={400}
+              width={400}
+              itemSize={80}
+              itemCount={5} /*Qui deve essere restituito il numero di match completati*/
+              overscanCount={5}
+            >
+              {renderMatchesRow}
+            </FixedSizeList>
+          </div>
+          <DialogActions style={{ justifyContent: 'center' }}>
+            <Button variant="outlined">Refresh</Button>
+            <Button variant="contained" onClick={handleCloseMatchesList}>Close</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
