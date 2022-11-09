69b201cb7a5470f784d3723417bfab0a966bdb86
Lorenzo Iafigliola <liafigli@e3r10p3.42roma.it>
2022-11-09 19:54:45 +0100

    Twofa component init

--- frontend/src/pages/Twofa.tsx
+++ frontend/src/pages/Twofa.tsx
@@ -1,15 +1,27 @@
 import {Header} from '../components/generic/Header';
+import React, { useState, useEffect } from "react";
 import '../font/font.css';
 import './css/Homepage.css';
+import { TwofaOn } from '../components/profile/TwofaOn';
 
 let logged = false;
 
 export const Twofa = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 	return (
         <>
         <Header/>
-        
+        <button onClick={handleOpen}>ciao</button>
+        <TwofaOn open={open} closeStatus={handleClose}/>
         </>
 	);
 }
