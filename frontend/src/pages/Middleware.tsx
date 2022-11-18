123268d376e0922e53c46dbcab739fc6142f5982
iafi <lorenzoiafi>
2022-11-18 15:50:47 +0100

    middleware fix style

--- frontend/src/pages/Middleware.tsx
+++ frontend/src/pages/Middleware.tsx
@@ -63,7 +63,7 @@ export const Middleware = () => {
                 body: JSON.stringify({ userName: nick.current.value })
             })
             const data = await response.json();
-            manageError(data, response, null ,setAlert)
+            manageError(data, response, null, setAlert)
             // const json = await response.json();
             // // console.log(json);
             // // setUser(json);
@@ -110,7 +110,88 @@ export const Middleware = () => {
 
     return (
         <>
-            <section className="vh-100" id="back" style={{ backgroundColor: '#282c34' }}>
+        <section className="vh-100" id="back" style={{ backgroundColor: '#282c34' }}>
+            <div className="container" style={{paddingTop: 300}}>
+                <div className="row d-flex justify-content-center align-items-center h-100"  style={{backgroundColor: 'white', borderRadius: 20}}>
+                    <div style={{ color: 'white', padding: 0}} className="col-12 col-md-6">
+                    <ImageUploading value={images}  onChange={onChange}>
+                            {({
+                                onImageUpload,
+                            }) => (
+                                // write your building UI
+                                <button onClick={onImageUpload} style={{ padding: 0, margin: 0, border: 'none', backgroundColor: 'transparent' }}>
+                                    <img src={user.img} width="100%" height="100%" style={{borderRadius: '20px 0px 0px 20px'}} alt="" />
+                                </button>
+                            )}
+                        </ImageUploading>
+                    </div>
+                    <div style={{ color: 'white' }} className="col-12 col-md-6">
+                        <div className="card-body p-4 p-lg-5 text-black">
+                            <form>
+                                <div className="align-items-center mb-3 pb-1" style={{ marginBottom: 0 }}>
+                                    <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                                    <span className="h1 fw-bold mb-0 center">Hello {user.idIntra}!</span>
+                                </div>
+                                <h5 className="align-items-center fw-normal mb-1 pb-1" style={{ letterSpacing: '1px', margin: '0' }}>Choose your nickname:</h5>
+                                <TextField label={user.userName} inputRef={nick} id="txtNick" variant="outlined" onBlur={clickSave} />
+                                <div className="pt-1 mb-4">
+                                    <button className="btn btn-dark btn-lg btn-block" style={{ marginTop: '10px', backgroundColor: '#1976d2', border: '2px solid #1976d2', marginRight: '2px' }} onClick={logFunction}>Next</button>
+                                    <button className="btn btn-dark btn-lg btn-block" style={{ marginTop: '10px', backgroundColor: 'transparent', border: '2px solid #1976d2', color: '#1976d2', marginLeft: '2px' }} onClick={logFunction}>Skip</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+            <Alert status={alert != "" ? true : false} closeStatus={() => setAlert("")} error={alert} />
+        </>
+    );
+}
+
+
+//container -> h-100
+//row -> d-flex justify-content-center align-items-center h-100
+
+
+{/* <section className="vh-100" id="back" style={{ backgroundColor: '#282c34' }}>
+            <div className="container " >
+                <div className="row"  style={{backgroundColor: 'white', borderRadius: 20}}>
+                    <div style={{ color: 'white', padding: 0}} className="col-12 col-md-6">
+                    <ImageUploading value={images}  onChange={onChange}>
+                            {({
+                                onImageUpload,
+                            }) => (
+                                // write your building UI
+                                <button onClick={onImageUpload} style={{ padding: 0, margin: 0, border: 'none', backgroundColor: 'transparent' }}>
+                                    <img src={user.img} width="100%" height="100%" style={{borderRadius: '20px 0px 0px 20px'}} alt="" />
+                                </button>
+                            )}
+                        </ImageUploading>
+                    </div>
+                    <div style={{ color: 'white' }} className="col-12 col-md-6">
+                        <div className="card-body p-4 p-lg-5 text-black">
+                            <form>
+                                <div className="align-items-center mb-3 pb-1" style={{ marginBottom: 0 }}>
+                                    <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
+                                    <span className="h1 fw-bold mb-0 center">Hello {user.idIntra}!</span>
+                                </div>
+                                <h5 className="align-items-center fw-normal mb-1 pb-1" style={{ letterSpacing: '1px', margin: '0' }}>Choose your nickname:</h5>
+                                <TextField label={user.userName} inputRef={nick} id="txtNick" variant="outlined" onBlur={clickSave} />
+                                <div className="pt-1 mb-4">
+                                    <button className="btn btn-dark btn-lg btn-block" style={{ marginTop: '10px', backgroundColor: '#1976d2', border: '2px solid #1976d2', marginRight: '2px' }} onClick={logFunction}>Next</button>
+                                    <button className="btn btn-dark btn-lg btn-block" style={{ marginTop: '10px', backgroundColor: 'transparent', border: '2px solid #1976d2', color: '#1976d2', marginLeft: '2px' }} onClick={logFunction}>Skip</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section> */}
+
+{/* <section className="vh-100" id="back" style={{ backgroundColor: '#282c34' }}>
                 <div className="container py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col col-xl-10">
@@ -153,8 +234,4 @@ export const Middleware = () => {
                         </div>
                     </div>
                 </div>
-            </section>
-            <Alert status={alert != "" ? true : false} closeStatus={() => setAlert("")} error={alert} />
-        </>
-    );
-}
+            </section> */}
