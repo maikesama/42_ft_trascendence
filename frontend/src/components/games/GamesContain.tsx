11a77f110912fe07a71cc39fbcf00cfa2f1a4115
ltorrean <ltorrean@student.42roma.it>
2022-11-21 21:51:56 +0000

    added basic fireworks

--- frontend/src/components/games/GamesContain.tsx
+++ frontend/src/components/games/GamesContain.tsx
@@ -18,6 +18,17 @@ import { useParams } from 'react-router';
 import { socket } from '../../App';
 
 // let ctx:any;
+
+var cw = window.innerWidth;
+var ch = window.innerHeight;
+
+// set canvas dimensions
+export const canvasDim = {
+  width: 1920,
+  height: 1080,
+}
+
+
 export const GamesContain = (props: any) => {
   const isSecondRender = useRef(false);
   // axios.get('api/getinfo').then(data=>data.json() )
@@ -76,287 +87,6 @@ export const GamesContain = (props: any) => {
     if (!ctx) {
       return;
     }
-
-    /*Effettooo*/
-    // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
-    // not supported in all browsers though and sometimes needs a prefix, so we need a shim
-    // window.requestAnimationFrame = (function () {
-    //   return window.requestAnimationFrame ||
-    //     window.requestAnimationFrame ||
-    //     window.requestAnimationFrame ||
-    //     function (callback) {
-    //       window.setTimeout(callback, 1000 / 60);
-    //     };
-    // })();
-
-    // // now we will setup our basic variables for the demo
-    //   var canvas2 = document.getElementById('canvas'),
-              
-    //   // full screen dimensions
-    //   cw = window.innerWidth,
-    //   ch = window.innerHeight,
-    //   // firework collection
-    //   fireworks: any[] = [],
-    //   // particle collection
-    //   particles: any[] = [],
-    //   // starting hue
-    //   hue = 120,
-    //   // when launching fireworks with a click, too many get launched at once without a limiter, one launch per 5 loop ticks
-    //   limiterTotal = 5,
-    //   limiterTick = 0,
-    //   // this will time the auto launches of fireworks, one launch per 80 loop ticks
-    //   timerTotal = 80,
-    //   timerTick = 0,
-    //   mousedown = false,
-    //   // mouse x coordinate,
-    //   mx: number,
-    //   // mouse y coordinate
-    //   my: number;
-
-    // // set canvas2 dimensions
-    // canvas2.width = cw ;
-    // canvas2.height = ch;
-
-    // // now we are going to setup our function placeholders for the entire demo
-
-    // // get a random number within a range
-    // function random(min: number, max: number) {
-    //   return Math.random() * (max - min) + min;
-    // }
-
-    // // calculate the distance between two points
-    // function calculateDistance(p1x: number, p1y: number, p2x: number, p2y: number) {
-    //   var xDistance = p1x - p2x,
-    //     yDistance = p1y - p2y;
-    //   return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-    // }
-
-    // // create firework
-    // function Firework(this: any, sx: number, sy: number, tx: number, ty: number) {
-    //   // actual coordinates
-    //   this.x = sx;
-    //   this.y = sy;
-    //   // starting coordinates
-    //   this.sx = sx;
-    //   this.sy = sy;
-    //   // target coordinates
-    //   this.tx = tx;
-    //   this.ty = ty;
-    //   // distance from starting point to target
-    //   this.distanceToTarget = calculateDistance(sx, sy, tx, ty);
-    //   this.distanceTraveled = 0;
-    //   // track the past coordinates of each firework to create a trail effect, increase the coordinate count to create more prominent trails
-    //   this.coordinates = [];
-    //   this.coordinateCount = 3;
-    //   // populate initial coordinate collection with the current coordinates
-    //   while (this.coordinateCount--) {
-    //     this.coordinates.push([this.x, this.y]);
-    //   }
-    //   this.angle = Math.atan2(ty - sy, tx - sx);
-    //   this.speed = 2;
-    //   this.acceleration = 1.05;
-    //   this.brightness = random(50, 70);
-    //   // circle target indicator radius
-    //   this.targetRadius = 1;
-    // }
-
-    // // update firework
-    // Firework.prototype.update = function (index: number) {
-    //   // remove last item in coordinates array
-    //   this.coordinates.pop();
-    //   // add current coordinates to the start of the array
-    //   this.coordinates.unshift([this.x, this.y]);
-
-    //   // cycle the circle target indicator radius
-    //   if (this.targetRadius < 8) {
-    //     this.targetRadius += 0.3;
-    //   } else {
-    //     this.targetRadius = 1;
-    //   }
-
-    //   // speed up the firework
-    //   this.speed *= this.acceleration;
-
-    //   // get the current velocities based on angle and speed
-    //   var vx = Math.cos(this.angle) * this.speed,
-    //     vy = Math.sin(this.angle) * this.speed;
-    //   // how far will the firework have traveled with velocities applied?
-    //   this.distanceTraveled = calculateDistance(this.sx, this.sy, this.x + vx, this.y + vy);
-
-    //   // if the distance traveled, including velocities, is greater than the initial distance to the target, then the target has been reached
-    //   if (this.distanceTraveled >= this.distanceToTarget) {
-    //     createParticles(this.tx, this.ty);
-    //     // remove the firework, use the index passed into the update function to determine which to remove
-    //     fireworks.splice(index, 1);
-    //   } else {
-    //     // target not reached, keep traveling
-    //     this.x += vx;
-    //     this.y += vy;
-    //   }
-    // }
-
-    // // draw firework
-    // Firework.prototype.draw = function () {
-    //   ctx.beginPath();
-    //   // move to the last tracked coordinate in the set, then draw a line to the current x and y
-    //   ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
-    //   ctx.lineTo(this.x, this.y);
-    //   ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
-    //   ctx.stroke();
-
-    //   ctx.beginPath();
-    //   // draw the target for this firework with a pulsing circle
-    //   ctx.arc(this.tx, this.ty, this.targetRadius, 0, Math.PI * 2);
-    //   ctx.stroke();
-    // }
-
-    // // create particle
-    // function Particle(this: any, x: any, y: any) {
-    //   this.x = x;
-    //   this.y = y;
-    //   // track the past coordinates of each particle to create a trail effect, increase the coordinate count to create more prominent trails
-    //   this.coordinates = [];
-    //   this.coordinateCount = 5;
-    //   while (this.coordinateCount--) {
-    //     this.coordinates.push([this.x, this.y]);
-    //   }
-    //   // set a random angle in all possible directions, in radians
-    //   this.angle = random(0, Math.PI * 2);
-    //   this.speed = random(1, 10);
-    //   // friction will slow the particle down
-    //   this.friction = 0.95;
-    //   // gravity will be applied and pull the particle down
-    //   this.gravity = 1;
-    //   // set the hue to a random number +-50 of the overall hue variable
-    //   this.hue = random(hue - 50, hue + 50);
-    //   this.brightness = random(50, 80);
-    //   this.alpha = 1;
-    //   // set how fast the particle fades out
-    //   this.decay = random(0.015, 0.03);
-    // }
-
-    // // update particle
-    // Particle.prototype.update = function (index: number) {
-    //   // remove last item in coordinates array
-    //   this.coordinates.pop();
-    //   // add current coordinates to the start of the array
-    //   this.coordinates.unshift([this.x, this.y]);
-    //   // slow down the particle
-    //   this.speed *= this.friction;
-    //   // apply velocity
-    //   this.x += Math.cos(this.angle) * this.speed;
-    //   this.y += Math.sin(this.angle) * this.speed + this.gravity;
-    //   // fade out the particle
-    //   this.alpha -= this.decay;
-
-    //   // remove the particle once the alpha is low enough, based on the passed in index
-    //   if (this.alpha <= this.decay) {
-    //     particles.splice(index, 1);
-    //   }
-    // }
-
-    // // draw particle
-    // Particle.prototype.draw = function () {
-    //   ctx.beginPath();
-    //   // move to the last tracked coordinates in the set, then draw a line to the current x and y
-    //   ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
-    //   ctx.lineTo(this.x, this.y);
-    //   ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
-    //   ctx.stroke();
-    // }
-
-    // // create particle group/explosion
-    // function createParticles(x: any, y: any) {
-    //   // increase the particle count for a bigger explosion, beware of the canvas2 performance hit with the increased particles though
-    //   var particleCount = 30;
-    //   while (particleCount--) {
-    //     particles.push(new Particle(x, y));
-    //   }
-    // }
-
-    // // main demo loop
-    // function loop() {
-    //   // this function will run endlessly with requestAnimationFrame
-    //   requestAnimationFrame(loop);
-
-    //   // increase the hue to get different colored fireworks over time
-    //   //hue += 0.5;
-
-    //   // create random color
-    //   hue = random(0, 360);
-
-    //   // normally, clearRect() would be used to clear the canvas2
-    //   // we want to create a trailing effect though
-    //   // setting the composite operation to destination-out will allow us to clear the canvas2 at a specific opacity, rather than wiping it entirely
-    //   ctx.globalCompositeOperation = 'destination-out';
-    //   // decrease the alpha property to create more prominent trails
-    //   ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-    //   ctx.fillRect(0, 0, cw, ch);
-    //   // change the composite operation back to our main mode
-    //   // lighter creates bright highlight points as the fireworks and particles overlap each other
-    //   ctx.globalCompositeOperation = 'lighter';
-
-    //   // loop over each firework, draw it, update it
-    //   var i = fireworks.length;
-    //   while (i--) {
-    //     fireworks[i].draw();
-    //     fireworks[i].update(i);
-    //   }
-
-    //   // loop over each particle, draw it, update it
-    //   var i = particles.length;
-    //   while (i--) {
-    //     particles[i].draw();
-    //     particles[i].update(i);
-    //   }
-
-    //   // launch fireworks automatically to random coordinates, when the mouse isn't down
-    //   if (timerTick >= timerTotal) {
-    //     if (!mousedown) {
-    //       // start the firework at the bottom middle of the screen, then set the random target coordinates, the random y coordinates will be set within the range of the top half of the screen
-    //       fireworks.push(new Firework(cw / 2, ch, random(0, cw), random(0, ch / 2)));
-    //       timerTick = 0;
-    //     }
-    //   } else {
-    //     timerTick++;
-    //   }
-
-    //   // limit the rate at which fireworks get launched when mouse is down
-    //   if (limiterTick >= limiterTotal) {
-    //     if (mousedown) {
-    //       // start the firework at the bottom middle of the screen, then set the current mouse coordinates as the target
-    //       fireworks.push(new Firework(cw / 2, ch, mx, my));
-    //       limiterTick = 0;
-    //     }
-    //   } else {
-    //     limiterTick++;
-    //   }
-    // }
-
-    // // mouse event bindings
-    // // update the mouse coordinates on mousemove
-    // canvas2.addEventListener('mousemove', function (e: { pageX: number; pageY: number; }) {
-    //   mx = e.pageX - canvas2.offsetLeft;
-    //   my = e.pageY - canvas2.offsetTop;
-    // });
-
-    // // toggle mousedown state and prevent canvas2 from being selected
-    // canvas2.addEventListener('mousedown', function (e: { preventDefault: () => void; }) {
-    //   e.preventDefault();
-    //   mousedown = true;
-    // });
-
-    // canvas2.addEventListener('mouseup', function (e: { preventDefault: () => void; }) {
-    //   e.preventDefault();
-    //   mousedown = false;
-    // });
-
-    // // once the window loads, we are ready for some fireworks!
-    // window.onload = loop;
-
-
-
-    /*EFFETTOOOOO*/
 
 
     const drawRect = (x: any, y: any, w: any, h: any, color: any) => {
@@ -442,7 +172,42 @@ export const GamesContain = (props: any) => {
         drawImg(powerUp.color, powerUp.x, powerUp.y, powerUp.width, powerUp.height);
       }
     }
-    // setEsit(null);
+
+    const fireworks = () => {
+      console.log("fireworks")
+      let particles: any;
+      let loop = setInterval(() => {
+        ctx.globalAlpha -= 0.01;
+        particles = [];
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < 1000; i++) {
+          particles.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            radius: Math.random() * 5,
+            color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+            vx: Math.random() * 10 - 5,
+            vy: Math.random() * 10 - 5,
+          });
+        }
+        console.log(particles)
+
+        particles.forEach((p: any) => {
+          ctx.beginPath();
+          ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
+          ctx.fillStyle = p.color;
+          ctx.fill();
+          ctx.closePath();
+          p.x += p.vx;
+          p.y += p.vy;
+        }
+        );
+
+        if (ctx.globalAlpha < 0.5) {
+          ctx.globalAlpha = 1;
+        }
+      }, 1000 / 60);
+    }
 
     socketGames.on('state', (gameState: any) => {
       start2.current = true;
@@ -463,6 +228,7 @@ export const GamesContain = (props: any) => {
       start2.current = false;
       setEsit(WinnerImage)
       console.log("win")
+      fireworks();
     });
 
     socketGames.on('GameNotFound', (gameState: any) => {
@@ -620,7 +386,7 @@ export const GamesContain = (props: any) => {
       {start && <Link key={"home"} component={RouterLink} to={"/"}>
         <button style={{ position: "absolute", top: "0", right: "0", zIndex: 1000, backgroundColor: "red", color: "white", fontSize: "20px" }} onClick={handleBack}>Quit</button>
       </Link>}
-      <canvas id="myCanvas" width="1920" height="1080" ref={canvasRef} />
+      <canvas id="myCanvas" width={canvasDim.width} height={canvasDim.height} ref={canvasRef} />
       {esit ? <div id="esit"><div><img src={esit} alt="lose" width="20%" height="20%" /></div><Link key={"home"} component={RouterLink} to={"/"}><button id="buttonGameHome">Home</button></Link><button id="buttonGameHome" onClick={handleRestart0}>Play Again Classic</button><button id="buttonGameHome" onClick={handleRestart1}>Play Again Custom</button></div> :
         null}
       {!start && !esit && <div id="textMatchmaking" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "black", color: "white !important" }}>
